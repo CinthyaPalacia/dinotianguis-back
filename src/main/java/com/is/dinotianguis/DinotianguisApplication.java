7e7442ae33ec3c1ae949d973dbceb442d65ce86a
paovats <paovats@ciencias.unam.mx>
2021-11-26 13:00:22 -0600

    init starter project

---
+++ src/main/java/com/is/dinotianguis/DinotianguisApplication.java
@@ -0,0 +1,13 @@
+package com.is.dinotianguis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinotianguisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinotianguisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/is/dinotianguis/DinotianguisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.dinotianguis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinotianguisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
